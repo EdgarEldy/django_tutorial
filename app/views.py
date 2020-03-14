f066850f4c8c0d510d6095decb8d18d5d390212c
Edgar MUHAMYANGABO <ed.eldy21@live.com>
2020-03-15 00:44:10 +0200

    Setting the home view

--- app/views.py
+++ app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpRequest
+from django.template import RequestContext
+from datetime import datetime
+
 
 # Create your views here.
+
+def home(request):
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/home/index.html',
+        {
+            'title': 'Django tutorial'
+        }
+    )
