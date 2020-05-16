02371b964178c69afbf0423722ebfd24e11320b8
Edgar MUHAMYANGABO <ed.eldy21@live.com>
2020-05-16 10:50:58 +0200

    Added Customer model

--- app/models.py
+++ app/models.py
@@ -3,5 +3,14 @@ from django.db import models
 
 # Create your models here.
 
+# Category model
 class Category(models.Model):
     cat_name = models.CharField(max_length=45)
+
+# Customer model
+class Customer(models.Model):
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)
+    email = models.CharField(max_length=45)
+    tel = models.CharField(max_length=45)
+    address = models.CharField(max_length=45)
