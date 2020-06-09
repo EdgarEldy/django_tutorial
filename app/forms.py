cd9f494cc373f4070c0d5d1d5d2320c100875b26
Edgar MUHAMYANGABO <ed.eldy21@live.com>
2020-06-09 22:10:02 +0200

    Added products modelForm

--- app/forms.py
+++ app/forms.py
@@ -5,18 +5,19 @@ Definition of forms.
 from django import forms
 from .models import Category
 from .models import Customer
+from .models import Product
 
 class CategoriesForm(forms.ModelForm):
     class Meta:
         model = Category
         fields = '__all__'
-        # cat_name = forms.CharField(max_length=254,
-        #                            widget=forms.TextInput({
-        #                                'class': 'form-control',
-        #                                'placeholder':'Category name'
-        #                            }))
 
 class CustomersForm(forms.ModelForm):
     class Meta:
         model = Customer
+        fields = '__all__'
+
+class ProductsForm(forms.ModelForm):
+    class Meta:
+        model = Product
         fields = '__all__'
