21db7565b6b67e072c5b45b9fa1448c7a1fb85ce
Edgar MUHAMYANGABO <ed.eldy21@live.com>
2020-06-09 22:07:17 +0200

    Added a magic method to retrieve category name

--- app/models.py
+++ app/models.py
@@ -7,6 +7,9 @@ from django.db import models
 class Category(models.Model):
     cat_name = models.CharField(max_length=45)
 
+    def __str__(self):
+        return self.cat_name
+
 # Customer model
 class Customer(models.Model):
     first_name = models.CharField(max_length=45)
