9d79172ca2cdb7aa9e7f7650975e3546ff70fd57
Edgar MUHAMYANGABO <ed.eldy21@live.com>
2020-09-21 21:53:51 +0200

    Fixed indents

--- django_tutorial/urls.py
+++ django_tutorial/urls.py
@@ -20,25 +20,25 @@ import app.forms
 import app.views
 
 urlpatterns = [
-   # path('admin/', admin.site.urls),
-     url(r'^$', app.views.home, name='home'),
-     url(r'^categories/$', app.views.categories_index, name='categories_index'),
-     url(r'^categories/add$', app.views.categories_add, name='categories_add'),
-     path('categories/edit/<int:id>', app.views.categories_edit, name='categories_edit'),
-     path('categories/delete/<int:id>', app.views.categories_delete, name='categories_delete'),
+    # path('admin/', admin.site.urls),
+    url(r'^$', app.views.home, name='home'),
+    url(r'^categories/$', app.views.categories_index, name='categories_index'),
+    url(r'^categories/add$', app.views.categories_add, name='categories_add'),
+    path('categories/edit/<int:id>', app.views.categories_edit, name='categories_edit'),
+    path('categories/delete/<int:id>', app.views.categories_delete, name='categories_delete'),
 
-     path('customers', app.views.customers_index, name='customers_index'),
-     path('customers/add', app.views.customers_add, name='customers_add'),
-     path('customers/edit/<int:id>', app.views.customers_edit, name='customers_edit'),
-     path('customers/delete/<int:id>', app.views.customers_delete, name='customers_delete'),
+    path('customers', app.views.customers_index, name='customers_index'),
+    path('customers/add', app.views.customers_add, name='customers_add'),
+    path('customers/edit/<int:id>', app.views.customers_edit, name='customers_edit'),
+    path('customers/delete/<int:id>', app.views.customers_delete, name='customers_delete'),
 
-     path('products',app.views.products_index, name='products_index'),
-     path('products/add', app.views.products_add, name='products_add'),
-     path('products/edit/<int:id>', app.views.products_edit, name='products_edit'),
-     path('products/delete/<int:id>', app.views.products_delete, name='products_delete'),
+    path('products',app.views.products_index, name='products_index'),
+    path('products/add', app.views.products_add, name='products_add'),
+    path('products/edit/<int:id>', app.views.products_edit, name='products_edit'),
+    path('products/delete/<int:id>', app.views.products_delete, name='products_delete'),
 
-     path('orders',app.views.orders_index, name='orders_index'),
-     path('orders/add', app.views.orders_add, name='orders_add'),
-     path('orders/edit/<int:id>', app.views.orders_edit, name='orders_edit'),
-     path('orders/delete/<int:id>', app.views.orders_delete, name='orders_delete'),
+    path('orders',app.views.orders_index, name='orders_index'),
+    path('orders/add', app.views.orders_add, name='orders_add'),
+    path('orders/edit/<int:id>', app.views.orders_edit, name='orders_edit'),
+    path('orders/delete/<int:id>', app.views.orders_delete, name='orders_delete'),
 ]
