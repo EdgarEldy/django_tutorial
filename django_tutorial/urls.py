cd09f260eae6c7b7b1e58a91e89e1f9c7193af82
Edgar MUHAMYANGABO <ed.eldy21@live.com>
2020-10-04 19:54:31 +0200

    Registered users url for showing users

--- django_tutorial/urls.py
+++ django_tutorial/urls.py
@@ -32,13 +32,16 @@ urlpatterns = [
     path('customers/edit/<int:id>', app.views.customers_edit, name='customers_edit'),
     path('customers/delete/<int:id>', app.views.customers_delete, name='customers_delete'),
 
-    path('products',app.views.products_index, name='products_index'),
+    path('products', app.views.products_index, name='products_index'),
     path('products/add', app.views.products_add, name='products_add'),
     path('products/edit/<int:id>', app.views.products_edit, name='products_edit'),
     path('products/delete/<int:id>', app.views.products_delete, name='products_delete'),
 
-    path('orders',app.views.orders_index, name='orders_index'),
+    path('orders', app.views.orders_index, name='orders_index'),
     path('orders/add', app.views.orders_add, name='orders_add'),
     path('orders/edit/<int:id>', app.views.orders_edit, name='orders_edit'),
     path('orders/delete/<int:id>', app.views.orders_delete, name='orders_delete'),
+
+    # users
+    path('users', app.views.users_index, name='users_index')
 ]
