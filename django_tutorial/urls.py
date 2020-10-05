6e197c46cdc54dcd61b5e0ebc05bc8cae6391412
Edgar MUHAMYANGABO <ed.eldy21@live.com>
2020-10-05 18:29:16 +0200

    Registered orders/getUnitPrice view

--- django_tutorial/urls.py
+++ django_tutorial/urls.py
@@ -42,6 +42,7 @@ urlpatterns = [
     path('orders/edit/<int:id>', app.views.orders_edit, name='orders_edit'),
     path('orders/delete/<int:id>', app.views.orders_delete, name='orders_delete'),
     path('orders/getProducts', app.views.getProducts, name='getProducts'),
+    path('orders/getUnitPrice', app.views.getUnitPrice, name='getUnitPrice'),
 
     # users
     path('users', app.views.users_index, name='users_index'),
