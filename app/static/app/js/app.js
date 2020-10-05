920cfdcb890102bfeaad7d96f48cf62ab02a13b7
Edgar MUHAMYANGABO <ed.eldy21@live.com>
2020-10-05 18:51:08 +0200

    Added an ajax request for getting unit price by product id using jquery

--- app/static/app/js/app.js
+++ app/static/app/js/app.js
@@ -1,3 +1,4 @@
+// Getting products by category_id
 $(function () {
     $('#category_id').on('change', function () {
         var category_id = $(this).val();
@@ -8,6 +9,20 @@ $(function () {
             },
             function (data) {
                 $('#id_product').html(data);
+            });
+    });
+});
+
+//Getting unit pricey by product id
+$(function () {
+    $('#id_product').on('change', function () {
+        var id_product = $(this).val();
+        $.get('/orders/getUnitPrice',
+            {
+                id_product: id_product
+            },
+            function (data) {
+                $('#unit_price').val(data);
             });
     });
 });
