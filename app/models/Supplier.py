1b3d1a0f5b99ccef91f04e6da5d1cac11dbfb0e0
Edgar Eldy <ed.eldy21@live.com>
2021-11-30 19:43:39 +0200

    Get supplier's first_name and last_name

--- app/models/Supplier.py
+++ app/models/Supplier.py
@@ -7,3 +7,7 @@ class Supplier(models.Model):
     tel = models.CharField(max_length=45)
     email = models.CharField(max_length=45)
     address = models.CharField(max_length=45)
+
+#Get supplier's first_name and last_name 
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
