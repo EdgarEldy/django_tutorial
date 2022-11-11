36aabf641c19108b694664cf8f5ddeabdc2972b7
EdgarEldy <ed.eldy21@live.com>
2022-11-11 21:13:47 +0200

    Remove urls

--- django_tutorial/urls.py
+++ django_tutorial/urls.py
@@ -13,47 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.contrib import admin
 from django.urls import path
 
-import app.forms
-import app.views
-
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    url(r'^$', app.views.home, name='home'),
-    url(r'^categories/$', app.views.categories_index, name='categories_index'),
-    url(r'^categories/add$', app.views.categories_add, name='categories_add'),
-    path('categories/edit/<int:id>', app.views.categories_edit, name='categories_edit'),
-    path('categories/delete/<int:id>', app.views.categories_delete, name='categories_delete'),
-
-    path('customers', app.views.customers_index, name='customers_index'),
-    path('customers/add', app.views.customers_add, name='customers_add'),
-    path('customers/edit/<int:id>', app.views.customers_edit, name='customers_edit'),
-    path('customers/delete/<int:id>', app.views.customers_delete, name='customers_delete'),
-
-    path('products', app.views.products_index, name='products_index'),
-    path('products/add', app.views.products_add, name='products_add'),
-    path('products/edit/<int:id>', app.views.products_edit, name='products_edit'),
-    path('products/delete/<int:id>', app.views.products_delete, name='products_delete'),
-
-    path('orders', app.views.orders_index, name='orders_index'),
-    path('orders/add', app.views.orders_add, name='orders_add'),
-    path('orders/edit/<int:id>', app.views.orders_edit, name='orders_edit'),
-    path('orders/delete/<int:id>', app.views.orders_delete, name='orders_delete'),
-    path('orders/getProducts', app.views.getProducts, name='getProducts'),
-    path('orders/getUnitPrice', app.views.getUnitPrice, name='getUnitPrice'),
-
-    # users
-    path('users', app.views.users_index, name='users_index'),
-    path('users/add', app.views.users_add, name='users_add'),
-    path('users/login', app.views.users_login, name='users_login'),
-    path('logout', app.views.users_logout, name='logout'),
-
-    # profiles
-    path('profiles', app.views.profiles_index, name='profiles_index'),
-    path('profiles/add', app.views.profiles_add, name='profiles_add'),
-    path('profiles/edit/<int:id>', app.views.profiles_edit, name='profiles_edit'),
-    path('profiles/delete/<int:id>', app.views.profiles_delete, name='profiles_delete'),
+    path('admin/', admin.site.urls),
 
 ]
