b799e597ce05b87e4b024f2b3a8b603f2b3f6e18
Edgar Eldy <ed.eldy21@live.com>
2022-11-24 17:17:46 +0200

    Remove models for rework

--- app/models/category.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Category model
-class Category(models.Model):
-    cat_name = models.CharField(max_length=45)
-
-    def __str__(self):
-        return self.cat_name
