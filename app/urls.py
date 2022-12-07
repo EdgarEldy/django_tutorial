6cdd97fbb21c8cf32efa954d1c5c0da769440206
Edgar Eldy <ed.eldy21@live.com>
2022-12-07 15:19:15 +0200

    Register products/edit/id url

--- app/urls.py
+++ app/urls.py
@@ -29,5 +29,6 @@ urlpatterns = [
     path('products/', products.index, name="products_index"),
     path('products/add', products.add, name="products_add"),
     path('products/store', products.store, name="products_store"),
+    path('products/edit/<int:id>', products.edit, name="products_edit"),
     
 ]
