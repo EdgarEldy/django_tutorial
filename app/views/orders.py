2322b0a7eff890b57d50043a35ccafa3fef94682
EdgarEldy <ed.eldy21@live.com>
2022-12-31 22:23:38 +0200

    Get orders page with categories

--- app/views/orders.py
+++ app/views/orders.py
@@ -2,8 +2,8 @@ from django.http import HttpRequest
 from django.shortcuts import redirect, render
 from django.contrib import messages
 
-from app.models import Order
-
+from app.models import Order, Category
+from app.forms import OrderForm
 # Show orders list
 def index(request):
     assert isinstance(request, HttpRequest)
@@ -13,5 +13,19 @@ def index(request):
         'app/orders/index.html',
         {
             'orders': orders
+        }
+    )
+    
+# Get orders page with categories
+def add(request):
+    assert isinstance(request, HttpRequest)
+    categories = Category.objects.all()
+    form = OrderForm()
+    return render(
+        request,
+        'app/orders/add.html',
+        {
+            'form': form,
+            'categories': categories
         }
     )
