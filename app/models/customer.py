dd5097e5c63047510e9fe4c1edea00c270763cd0
Edgar Eldy <ed.eldy21@live.com>
2023-04-21 21:51:54 +0200

    Change to telephone property

--- app/models/customer.py
+++ app/models/customer.py
@@ -3,6 +3,6 @@ from django.db import models
 class Customer(models.Model):
     first_name = models.CharField(max_length=45)
     last_name = models.CharField(max_length=45)
-    tel = models.CharField(max_length=45)
+    telephone = models.CharField(max_length=45, null=True)
     email = models.CharField(max_length=45)
     address = models.CharField(max_length=45)
